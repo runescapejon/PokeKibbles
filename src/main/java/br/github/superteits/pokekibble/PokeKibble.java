a1eb7646fb94b9fd59784029135e245fe175ed83
runescapejon <jonsimpletv@gmail.com>
2019-02-08 04:59:28 -0500

    Support 7.0.1

--- src/main/java/br/github/superteits/pokekibble/PokeKibble.java
+++ src/main/java/br/github/superteits/pokekibble/PokeKibble.java
@@ -33,7 +33,7 @@ import java.nio.file.Path;
         version = PokeKibble.VERSION,
         authors = "Teits",
         description = PokeKibble.DESCRIPTION,
-        dependencies = @Dependency(id = "pixelmon", version = "[7.0.0]"))
+        dependencies = @Dependency(id = "pixelmon", version = "[7.0.1]"))
 public class PokeKibble {
 
     public static final String ID = "pokekibble";
